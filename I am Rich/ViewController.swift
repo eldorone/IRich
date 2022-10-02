461dcf7eb0550b7df1b10c84b893e29e078a23ed
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-02 18:17:09 +0900

    i-am-rich-app

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Eldor Alikuvatov on 2021/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
