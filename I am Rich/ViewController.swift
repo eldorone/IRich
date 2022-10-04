8bf37e57b131770b00462b12375b746ccc883806
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-04 23:22:50 +0900

    Update ViewController.swift

--- I am Rich/ViewController.swift
+++ I am Rich/ViewController.swift
@@ -1,1 +1,22 @@
 
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Eldor Alikuvatov on 2021/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World!")
+        print("I am rich!")
+    }
+
+
+}
+
