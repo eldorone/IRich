ec77979fe0950e70ef22803172bee2f3f574d121
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-18 23:54:47 +0900

    Update ViewController.swift

--- I am Rich/ViewController.swift
+++ I am Rich/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Hello World")
+        print("This is swift app practice")
         print("I am Rich!")
         print("This is the application for rich people.")
     }
