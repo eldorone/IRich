4946a8a4ea1c50f56ec539e6651fa49254f57b14
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-10-25 23:35:37 +0900

    Update ViewController.swift

--- I am Rich/ViewController.swift
+++ I am Rich/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         
         print("I am Rich!")
         print("This is the application for rich people.")
+        print("Just pull the repo and have fun with that..")
     }
 
 
